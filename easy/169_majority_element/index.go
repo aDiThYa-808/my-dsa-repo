68cb879cf0deb635edd35455570c4e3aaca8f03d
aDiThYa-808 <adithyas824@gmail.com>
2025-09-23 23:45:04 +0530

    easy: 169:Majority Element

---
+++ easy/169_majority_element/index.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 1, 1, 2, 1}
+	fmt.Println("Majority element in ", nums, " is :", majorityElement(nums)) //1
+}
+
+func majorityElement(nums []int) int {
+	var count int
+	var candidate int
+
+	for i := 0; i < len(nums); i++ {
+		if count == 0 {
+			candidate = nums[i]
+		}
+		if nums[i] == candidate {
+			count++
+		} else {
+			count--
+		}
+	}
+	return candidate
+}
+
+// Notes:
+// 1. For [1 2 3 1 1 2 1], majorityElement does the following:
+// See 1 → count=0 → candidate=1, count=1
+// See 2 → 2 != 1 → count=0
+// See 3 → count=0 → candidate=3, count=1
+// See 1 → 1 != 3 → count=0 → candidate=1, count=1
+// See 1 → 1 == 1 → count=2
+// See 2 → 2 != 1 → count=1
+// See 1 → 1 == 1 → count=2
